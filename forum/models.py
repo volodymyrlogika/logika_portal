8d5b3beab58b8f02bd2864d305dc46c718a08ddd
KuthikBohdan1 <bkuzik14@gmail.com>
2025-08-26 23:16:29 +0300

    add base models

--- forum/models.py
+++ forum/models.py
@@ -1,4 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+class Category(models.Model):
+    CATEGORY_TYPE_CHOICES = [
+        ('games', 'Ігри'),
+        ('code', 'Програмування'),
+        ('music', 'Музика'),
+        ('sport', 'Спорт'),
+        ('other', 'Інше'),
+        ]
+    
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    type = models.CharField(max_length=35, choices=CATEGORY_TYPE_CHOICES)
+
+
+class Thread(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="branches")
+
+
+class Post(models.Model):
+    branch = models.ForeignKey(Thread, on_delete=models.CASCADE)
