1079c5af1f88ffa5d7f30813b5a263e12a73f8e0
KuthikBohdan1 <bkuzik14@gmail.com>
2025-08-28 20:06:23 +0300

    add all models

--- forum/models.py
+++ forum/models.py
@@ -14,11 +14,22 @@ class Category(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
     type = models.CharField(max_length=35, choices=CATEGORY_TYPE_CHOICES)
-
+    image = models.ImageField(upload_to="categories/", null=True, blank=True)
+    def __str__(self):
+        return self.name
 
 class Thread(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="threads")
-
+    theme = models.CharField(max_length=300)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    date_start = models.DateTimeField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    image = models.ImageField(upload_to="Thread", null=True, blank=True)
 
 class Post(models.Model):
-    branch = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_text = models.TextField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    files = models.FileField(upload_to="posts/", null=True, blank=True)
--- forum/views.py
+++ forum/views.py
@@ -1,1 +1,5 @@
 from django.shortcuts import render
+from models import Post,Thread,Category
+
+
+class CategoryL
