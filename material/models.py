979ca9a3cefee40fcf880dbb788c53c0cbb65cd6
pider485 <oleksijzolotarov4@gmail.com>
2025-08-30 00:38:41 +0200

    add models(advert_id, name, fille, image, yt_url, creater, ceat_time, Update_time)

--- material/models.py
+++ material/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Material(models.Model):
+    advert_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+    fille = models.FileField()
+    image = models.ImageField()
+    yt_url = models.CharField(max_length=80)
+    creater = models.CharField(max_length=30)
+    creat_time = models.DateTimeField(auto_now_add=True)
+    Update_time = models.DateTimeField(auto_now_add=True)
