9a3fa01a870f8d650a628ca3ade93248cfe043fc
kinobrac-sys <kinobrac.github@gmail.com>
2025-08-30 11:19:55 +0300

    Ukr lang models upd

--- event/models.py
+++ event/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Event(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    date = models.DateField()
+    title = models.CharField(max_length=200, verbose_name='Назва події')
+    description = models.TextField(null=True, blank=True, verbose_name='Опис події')
+    date = models.DateField(verbose_name='Дата події')
     speciality = (
         ("-", 'none'),
         ('+-', "soso"),
         ('+', "indeed"),
         ('++', "VERY NEED")
     )
-    speciality = models.CharField(choices=speciality, default="-")
-    user = models.ForeignKey(User, on_delete=models.CASCADE)    
+    speciality = models.CharField(choices=speciality, default="-", verbose_name='Важливість події')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Користувач')    
