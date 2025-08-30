076b606b805104118e4b762069e2d479b956fb52
pider485 <oleksijzolotarov4@gmail.com>
2025-08-30 10:18:48 +0200

    update db (material)

--- material/models.py
+++ material/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Material(models.Model):
-    advert_id = models.IntegerField()
-    name = models.CharField(max_length=50)
-    fille = models.FileField(upload_to='material/file',null=True,blank=True)
-    image = models.ImageField(upload_to='material/image',null=True,blank=True)
-    yt_url = models.CharField(max_length=80,null=True,blank=True)
-    creator = models.CharField(max_length=30)
-    creat_time = models.DateTimeField(auto_now_add=True)
-    Update_time = models.DateTimeField(auto_now_add=True)
-    description = models.TextField(null=True,blank=True)
+    name = models.CharField(max_length=50, verbose_name='Назва матеріалу')
+    description = models.TextField(null=True,blank=True,verbose_name='Опис')
+    file = models.FileField(upload_to='material/file',null=True,blank=True,verbose_name='Файл')
+    image = models.ImageField(upload_to='material/image',null=True,blank=True,verbose_name='Фото')
+    yt_url = models.CharField(max_length=250,null=True,blank=True,verbose_name='Посилання ют')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE,verbose_name='Автор')
+    creat_time = models.DateTimeField(auto_now_add=True,verbose_name='Час створення')
+    update_time = models.DateTimeField(auto_now_add=True,verbose_name='Час оновлення')
+    
+    def __str__(self):
+        return self.name
