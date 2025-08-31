d225705c51ff3b5bcd9f4115f8bf39e2ed8bccb1
KuthikBohdan1 <bkuzik14@gmail.com>
2025-08-31 19:21:57 +0300

    add new views

--- forum/urls.py
+++ forum/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
    
-    path('categories/', views.CategoryListView.as_view()),
-    path('threads/', views.ThreadListView.as_view()),
+    path('categories/', views.AllCategoryListView.as_view()),
+    path('threads/', views.AllThreadListView.as_view()),
     path('home/', views.ForumHomeView.as_view()),
 
 ]
--- forum/views.py
+++ forum/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect
 from .models import Post, Thread, Category
 from django.views.generic import ListView, DetailView, CreateView, View, UpdateView, DeleteView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from forum import models
 
-
-class CategoryListView(ListView):
+class AllCategoryListView(ListView):
     model = Category
     context_object_name = "categories"
     template_name = "forum/category_list.html"
@@ -13,10 +13,10 @@ class CategoryMixin:
     def get_categories(self):
         return Category.objects.all()
 
-class ThreadListView(ListView):
+class AllThreadListView(ListView):
     model = Thread
     context_object_name = "threds"
-    template_name = "forum/thread_list.html"
+    template_name = "forum/all_thread_list.html"
 
 class ThreadMixin:
     def get_threads(self):
@@ -32,4 +32,26 @@ class ForumHomeView(ThreadMixin, CategoryMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         context['categories'] = self.get_categories()
         context['threads'] = self.get_threads()
+        return context
+#########################################
+
+class ThreadListView(ListView):
+    model = Thread
+    context_object_name = "threads"
+    template_name = "thread_list.html"
+
+    def get_queryset(self):
+        return super().get_queryset()
+    
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+class PostLits(ListView):
+    model = models.Post
+    context_object_name = "posts"
+    template_name = "forum/post_list.html"
+
+    def get_context_data(self,*args, **kwargs):
+        context = super().get_context_data(**kwargs):
+        context
         return context
