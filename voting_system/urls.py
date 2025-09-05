b5a1c877cba264f8a31ff09e43b1928c3bd1dcab
Nazar Karpa <Bonasane@gmail.com>
2025-09-05 15:57:07 +0300

    add votelist and beta votedetail

--- voting_system/urls.py
+++ voting_system/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-
+    path('', views.VoteListView.as_view(), name='vote-list'),
+    path('<int:pk>/', views.VoteDetailView.as_view(), name ='voting-detail'),
 ]
--- voting_system/views.py
+++ voting_system/views.py
@@ -1,10 +1,27 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, DetailView, DeleteView
 
+from voting_system.mixins import UserIsOwnerMixins
 from voting_system.models import Voting
 
 
 class VoteListView(LoginRequiredMixin, ListView):
     model = Voting
+    context_object_name = 'votes'
     template_name = 'voting_list.html'
+
+
+class VoteDetailView(LoginRequiredMixin, DetailView):
+    model = Voting
+    context_object_name = 'vote'
+    template_name = 'voting_detail.html'
+
+
+class VoteCreateViwe(LoginRequiredMixin, CreateView):
+    pass
+
+
+
+class VoteDeleteView(LoginRequiredMixin, UserIsOwnerMixins, DeleteView):
+    pass
