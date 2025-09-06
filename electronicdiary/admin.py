3387fffc9f65f0937369be8d397714f4ef9cbc66
Miral1t <stankourij17@gmail.com>
2025-09-06 11:37:42 +0300

    Створив Базу данних

--- electronicdiary/admin.py
+++ electronicdiary/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Lesson,Logik
 
-# Register your models here.
+admin.site.register(Lesson)
+admin.site.register(Logik)
+
--- electronicdiary/models.py
+++ electronicdiary/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Lesson(models.Model):
+    title = models.CharField(max_length=100,verbose_name="Назва уроку")
+    datetime = models.DateTimeField(verbose_name="Дата Уроку")
 
+class Logik(models.Model):
+    student = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name="Учень") 
+    lesson = models.ForeignKey(Lesson,on_delete=models.CASCADE,verbose_name="Урок")
+    grade = models.IntegerField(verbose_name="Оцінка")           
+    date_creation= models.DateTimeField(auto_now_add=True,verbose_name="Дата")
