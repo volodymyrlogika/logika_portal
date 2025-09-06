b7a0e55f18de719e21762465980802c2706df36d
Miral1t <stankourij17@gmail.com>
2025-09-06 12:19:41 +0300

    вюв ще не дописаний але щось є

--- electronicdiary/views.py
+++ electronicdiary/views.py
@@ -1,2 +1,16 @@
 from django.shortcuts import render
- 
+
+from lesson.models import Lesson
+
+def room_list(request):
+    rooms = Room.objects.all()
+    context = {
+        'rooms': rooms
+    }
+    return render(request, template_name="room_list.html", context=context)
+def Lesson(request,id_room):
+    Lessons = get_object_or_404(Lesson,id = id_room)
+    context = {
+        'room': room_list
+    }
+    return render(request, template_name='booking.html',context=context)
