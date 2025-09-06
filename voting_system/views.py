fa3b2935db4735765ace21918b453af5250b4701
Nazar Karpa <Bonasane@gmail.com>
2025-09-06 11:08:14 +0300

    fix mini bug

--- voting_system/views.py
+++ voting_system/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, DetailView, DeleteView
 
 from voting_system.mixins import UserIsOwnerMixins
-from voting_system.models import Voting
+from voting_system.models import *
 
 
 class VoteListView(LoginRequiredMixin, ListView):
@@ -17,8 +17,25 @@ class VoteDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'vote'
     template_name = 'voting_system/voting_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        voting = self.get_object()
+        vote = Vote.objects.filter(voting=voting, user=self.request.user).first()
+        if vote:
+            context['user_voted'] = vote
+        return context
 
-class VoteCreateViwe(LoginRequiredMixin, CreateView):
+
+    def post(self, request, *args, **kwargs):
+        choice = request.POST.get('choice')
+        option = Option.objects.get(id=choice)
+        vote = Vote(user=self.request.user,
+                    option_vote=option,
+                    voting=self.get_object())
+
+        return redirect('vote-list')
+
+class VoteCreateView(LoginRequiredMixin, CreateView):
     pass
 
 
