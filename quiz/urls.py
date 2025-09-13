d33e25fa58f79e83fd05a3a7b9410bd79f67c9b9
sem_naz <semerenkonazar90@gmail.com>
2025-09-13 12:32:10 +0300

    add quiz list, and also

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("quiz/<int:quiz_id>/", views.take_quiz, name="take_quiz"),
     path("quiz/<int:quiz_id>/result/", views.quiz_result, name="quiz_result"),
+    path('quizzes/', views.quiz_list, name='quiz_list'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Quiz, Question, AnswerOption, UserAnswer
 
 @login_required
+def quiz_list(request):
+    quizzes = Quiz.objects.all()
+    return render(request, 'quiz/quiz_list.html', {'quizzes': quizzes})
+
+@login_required
 def take_quiz(request, quiz_id):
     quiz = get_object_or_404(Quiz, id=quiz_id)
-    questions = Question.question.all()
+    # отримаємо всі питання цього тесту
+    questions = quiz.Запитання.all()  
 
     if request.method == 'POST':
+        # видаляємо попередні відповіді користувача на цей тест
         UserAnswer.objects.filter(user=request.user, survey=quiz).delete()
 
         for question in questions:
@@ -33,6 +40,7 @@ def take_quiz(request, quiz_id):
         return redirect("quiz_result", quiz_id=quiz.id)
 
     return render(request, "quiz/take_quiz.html", {"quiz": quiz, "questions": questions})
+
 
 @login_required
 def quiz_result(request, quiz_id):
