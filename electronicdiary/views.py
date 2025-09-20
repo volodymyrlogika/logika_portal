e9192acbb3fe54de169b910cb67d6b773599a972
Miral1t <stankourij17@gmail.com>
2025-09-20 12:26:17 +0300

    індекс ліст хтмл

--- electronicdiary/views.py
+++ electronicdiary/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 
-from lesson.models import Lesson
+def index(request):
+    return render(request, 'index.html')
 
-def room_list(request):
-    rooms = Room.objects.all()
-    context = {
-        'rooms': rooms
-    }
-    return render(request, template_name="room_list.html", context=context)
-def Lesson(request,id_room):
-    Lessons = get_object_or_404(Lesson,id = id_room)
-    context = {
-        'room': room_list
-    }
-    return render(request, template_name='booking.html',context=context)
+def grades_list(request):
+    grades = [
+        {"student_name": "", "subject": "Математика", "score": 11, "date": "2025-09-19"},
+        {"student_name": "", "subject": "Фізика", "score": 12, "date": "2025-09-18"},
+    ]
+    return render(request, 'list.html', {'grades': grades})
+
+
+
