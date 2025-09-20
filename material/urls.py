6458cd2730f6f7ce7fbebb64b199210e6a996d70
volodymyr <volodymyrlogika@gmail.com>
2025-09-20 11:46:30 +0300

    fix urls

--- material/urls.py
+++ material/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
    
-    path('material/', views.material, name='material'),
-    path('add_material/', views.add_material, name='add_material'),
-    path('delet_material/<int:material_id>/', views.delet_material, name='delet_material'),
+    path('', views.material, name='material'),
+    path('add/', views.add_material, name='add_material'),
+    path('delete/<int:material_id>/', views.delet_material, name='delet_material'),
 ]
